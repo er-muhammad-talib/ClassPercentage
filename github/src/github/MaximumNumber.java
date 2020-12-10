bc39d5537d15a9754f8ddc0af2cd1ba1cfc23322
Muhammad <khantalib707@gmail.com>
2020-12-10 14:16:11 +0530

    arrey

--- github/src/github/MaximumNumber.java
+++ github/src/github/MaximumNumber.java
@@ -1,10 +1,35 @@
 package github;
 
+import java.util.Scanner;
+
 public class MaximumNumber {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        public static void main(String[] args) {
+		Scanner obj = new Scanner(System.in);
+		System.out.println("How many number in arrey:");
+		int n = obj.nextInt();
+		int[] arr = new int[n];
+		
+		for(int i = 0 ; i < n ; i++) {
+			
+			arr[i]  = obj.nextInt();
+		}
+		
+		System.out.println("you enter number:");
+		for(int i = 0 ; i < n ; i++) {
+			System.out.println(arr[i]);
+		}
+		
+		int max = arr[0];
+		for(int i = 1 ; i < arr.length ; i++ ) {
+			
+			if(arr[i] > max) {
+				
+				max = arr[i];
+			}
+		}
+		System.out.println("Maximum Number is:" + max);
 
-	}
-
+	
 }
+
